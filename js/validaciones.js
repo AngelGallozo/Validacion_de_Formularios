839191690a83e9f9ab3f7622ba8c0eb8c8157085
AngelGallozo <luisangelgallozo@gmail.com>
2022-10-31 19:37:18 -0300

    Generando las validaciones en js

---
+++ js/validaciones.js
@@ -0,0 +1,31 @@
+const inputNacimiento = document.querySelector('#birth');
+inputNacimiento.addEventListener('blur',(event)=>{
+    validarNacimiento(event.target);
+});
+
+
+function validarNacimiento(input){
+    const fechaCliente = new Date(input.value);
+    let mensaje = "";
+    if(!mayorDeEdad(fechaCliente)){
+        mensaje="Debes tener al menos 18 años de edad";
+    }
+    input.setCustomValidity(mensaje);
+}
+
+function mayorDeEdad(fecha){
+    const fechaActual = new Date()
+    const diferenciaFechas = new Date(fecha.getUTCFullYear() + 18,fecha.getUTCMonth(), fecha.getUTCDate());
+    return diferenciaFechas <= fechaActual;
+}
+
+export function valida(input){
+    const tipoDeInput = input.dataset.tipo;
+    if(validadores[tipoDeInput]){
+        validadores[tipoDeInput](input);
+    }
+}
+
+const validadores = {
+    nacimiento: (input) => validadNacimiento(input),
+}
